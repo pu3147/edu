99aabd390465879ec96366aad3cc4f1ecef4e303
gaopan@shangzhuang <gao9929@163.com>
2018-03-14 23:02:03 +0800

    第一个C程序

---
+++ c_program/day01/struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+struct Books
+{
+	char title[10];
+	char author[10];
+	char subject[100];
+	int book_id;
+};
+
+int main(){
+	struct Books book1;
+	struct Books book2;
+	strcpy(book1.title,"第一个");
+
+	printf("book1.title :%s",book1.title);
+
+}
