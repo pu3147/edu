6158eafe4b989f56c0bb6686d6a435f0fc29d2f9
gaopan@chemcn <gao9929@163.com>
2018-03-21 15:49:12 +0800

    SpringBoot

--- java_program/springBoot/src/main/java/cc/fbsky/edu/springBoot/conroller/main/StartApp.java
+++ java_program/springBoot/src/main/java/cc/fbsky/edu/springBoot/conroller/main/StartApp.java
@@ -1,12 +1,38 @@
 package cc.fbsky.edu.springBoot.conroller.main;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-import cc.fbsky.edu.springBoot.conroller.HelloController;
-
+@SpringBootApplication
 public class StartApp
 {
-	public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloController.class, args);
-    }
+	static final Logger LOG = LoggerFactory.getLogger(StartApp.class);
+	
+	public static void main(String[] args) throws Exception
+	{
+		SpringApplication.run(StartApp.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx)
+	{
+		return args ->
+		{
+			LOG.debug("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames)
+			{
+				LOG.debug(beanName);
+			}
+		};
+	}
+	
 }
