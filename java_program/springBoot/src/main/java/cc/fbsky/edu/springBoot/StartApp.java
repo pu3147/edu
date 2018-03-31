ddf57275aa026f855e70136f2c99db25d51d1152
gaopan@shangzhuang <gao9929@163.com>
2018-03-31 21:36:07 +0800

    Redid

--- java_program/springBoot/src/main/java/cc/fbsky/edu/springBoot/StartApp.java
+++ java_program/springBoot/src/main/java/cc/fbsky/edu/springBoot/StartApp.java
@@ -1,18 +1,31 @@
 package cc.fbsky.edu.springBoot;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class StartApp extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(StartApp.class);
-    }
+public class StartApp {
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(StartApp.class, args);
-    }
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(StartApp.class, args);
+	}
 }
