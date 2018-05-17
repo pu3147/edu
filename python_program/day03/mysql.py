696361f0dfe42e7c997c5109a6cca971ce568c29
流火 <gao9929@gmail.com>
2018-05-17 14:17:02 +0800

    python,mysql
    python,mysql

---
+++ python_program/day03/mysql.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import pymysql
+db = pymysql.connect("127.0.0.1","pu3147","1qaz@WSX","pu3147",charset='UTF8')
+
+cur = db.cursor();
+cur.execute("select member_id,member_name from t_mem_member order by member_id limit 100");
+# 使用 fetchone() 方法获取一条数据
+memberList = cur.fetchmany(100)
+print(memberList)
+for result in memberList:
+	memberId = result[0]
+	memberName = result[1]
+#	print(memberId,memberName)
+# 关闭数据库连接
+
+#另外一个查询
+cur.execute("select member_id,member_name from t_mem_member order by member_id limit 2");
+data = cur.fetchone()
+print("data>>>>>",data)
+db.close()
