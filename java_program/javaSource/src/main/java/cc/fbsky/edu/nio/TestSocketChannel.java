9a1f40d2d61c87b9ce80fcb7f22199969f9eed2c
freeLess <gao9929@163.com>
2021-03-28 21:42:25 +0800

    NIO

--- java_program/javaSource/src/main/java/cc/fbsky/edu/nio/TestSocketChannel.java
+++ java_program/javaSource/src/main/java/cc/fbsky/edu/nio/TestSocketChannel.java
@@ -15,9 +15,16 @@ public class TestSocketChannel {
 
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.socket().bind(new InetSocketAddress(9999));
+        //ServerSocketChannel可以设置成非阻塞模式。
+        // 在非阻塞模式下，accept() 方法会立刻返回，如果还没有新进来的连接,返回的将是null。
+        // 因此，需要检查返回的SocketChannel是否是null
+        serverSocketChannel.configureBlocking(false);
+
         while (true){
             SocketChannel socketChannel = serverSocketChannel.accept();
-            readSocketChannel(socketChannel);
+            if(null != serverSocketChannel) {
+                readSocketChannel(socketChannel);
+            }
             try {
                 Thread.currentThread().sleep(10000L);
             } catch (InterruptedException e) {
